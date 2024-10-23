ea4c9aaed410d85da22412a4d1e410f9eadc4264
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-10-23 15:28:40 -0400

    updating docs-builder

--- terraform/spoke-k8s_cluster.tf
+++ terraform/spoke-k8s_cluster.tf
@@ -266,8 +266,9 @@ resource "azurerm_kubernetes_flux_configuration" "flux_configuration" {
     reference_value          = data.git_repository.current.branch
     sync_interval_in_seconds = 60
     #local_auth_reference     = kubernetes_secret.github_auth_secret.metadata[0].name
-    https_user = "robinmordasiewicz"
-    https_key_base64 = base64encode(var.github_token)
+    #https_user = "robinmordasiewicz"
+    #https_key_base64 = base64encode(var.github_token)
+    ssh_private_key_base64 = base64encode(var.control_repo_ssh_private_key)
   }
   kustomizations {
     name                       = "infrastructure"

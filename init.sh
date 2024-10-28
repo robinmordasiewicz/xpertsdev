201f982644df26ce1bdabbec7939e025c5936738
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-10-28 13:11:09 -0400

    updating docs-builder

--- init.sh
+++ init.sh
@@ -508,12 +508,13 @@ copy_docs-builder-workflow_to_docs-builder_repo() {
   clone_commands+="        run: |\n"
   clone_commands+="          if [ -f ~/.ssh/id_ed25519 ]; then chmod 600 ~/.ssh/id_ed25519; fi\n"
   clone_commands+="          echo '\${{ secrets.${landing_page_secret_key_name}}}' > ~/.ssh/id_ed25519 && chmod 400 ~/.ssh/id_ed25519\n"
-  clone_commands+="          git clone git@github.com:\${{ github.repository_owner }}/${LANDING_PAGE_REPO_NAME}.git \$TEMP_DIR/docs\n\n"
+  clone_commands+="          mkdir $TEMP_DIR/landing-page\n"
+  clone_commands+="          git clone git@github.com:\${{ github.repository_owner }}/${LANDING_PAGE_REPO_NAME}.git \$TEMP_DIR/landing-page/docs\n\n"
 
   clone_commands+="      - name: Link mkdocs.yml\n"
   clone_commands+="        shell: bash\n"
   clone_commands+="        run: |\n"
-  clone_commands+="          echo 'INHERIT: docs/theme/mkdocs.yml' > \$TEMP_DIR/mkdocs.yml\n\n"
+  clone_commands+="          echo 'INHERIT: docs/theme/mkdocs.yml' > \$TEMP_DIR/landing-page/mkdocs.yml\n\n"
 
   local theme_secret_key_name="$(echo "${THEME_REPO_NAME}" | tr '[:lower:]-' '[:upper:]_')_SSH_PRIVATE_KEY"
   clone_commands+="      - name: Clone Theme\n"
@@ -521,8 +522,8 @@ copy_docs-builder-workflow_to_docs-builder_repo() {
   clone_commands+="        run: |\n"
   clone_commands+="          if [ -f ~/.ssh/id_ed25519 ]; then chmod 600 ~/.ssh/id_ed25519; fi\n"
   clone_commands+="          echo '\${{ secrets.${theme_secret_key_name}}}' > ~/.ssh/id_ed25519 && chmod 400 ~/.ssh/id_ed25519\n"
-  clone_commands+="          git clone git@github.com:\${{ github.repository_owner }}/${THEME_REPO_NAME}.git \$TEMP_DIR/docs/theme\n"
-  clone_commands+="          docker run --rm -v \$TEMP_DIR:/docs \${{ secrets.MKDOCS_REPO_NAME }} build -c -d site/\n\n"
+  clone_commands+="          git clone git@github.com:\${{ github.repository_owner }}/${THEME_REPO_NAME}.git \$TEMP_DIR/landing-page/docs/theme\n"
+  clone_commands+="          docker run --rm -v \$TEMP_DIR/landing-page:/docs \${{ secrets.MKDOCS_REPO_NAME }} build -c -d site/\n\n"
 
   clone_commands+="      - name: Clone Content Repos\n"
   clone_commands+="        shell: bash\n"
@@ -538,7 +539,7 @@ copy_docs-builder-workflow_to_docs-builder_repo() {
     clone_commands+="          cp -a \$TEMP_DIR/docs/theme \$TEMP_DIR/src/${repo}/docs/\n"
     clone_commands+="          echo 'INHERIT: docs/theme/mkdocs.yml' > \$TEMP_DIR/src/${repo}/mkdocs.yml\n"
     clone_commands+="          docker run --rm -v \$TEMP_DIR/src/${repo}:/docs \${{ secrets.MKDOCS_REPO_NAME }} build -d site/\n"
-    clone_commands+="          mv \$TEMP_DIR/src/${repo}/site \$TEMP_DIR/docs/site/${repo}\n\n"
+    clone_commands+="          mv \$TEMP_DIR/src/${repo}/site \$TEMP_DIR/landing-page/site/${repo}\n\n"
   done
 
   echo -e "$clone_commands" | sed -e "/%%INSERTCLONEREPO%%/r /dev/stdin" -e "/%%INSERTCLONEREPO%%/d" "$tpl_file" | awk 'BEGIN { blank=0 } { if (/^$/) { blank++; if (blank <= 1) print; } else { blank=0; print; } }' > "$output_file"
